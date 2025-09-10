f85cb9e7653631b2df7684210b607db18d612064
nema-develop <microayatron@gmail.com>
2025-09-10 19:58:40 +0900

    [fcm]firebase-config.js を追加（SW向けの公開設定）
    - self.firebaseConfig に Web アプリの公開設定を追加
    - Service Worker の initializeApp で参照

---
+++ public/firebase-config.js
@@ -0,0 +1,10 @@
+// Firebase Console > プロジェクト設定 > 一般 > SDKの設定 から取得
+self.firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_PROJECT.firebaseapp.com",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_PROJECT.appspot.com",
+    messagingSenderId: "YOUR_SENDER_ID",
+    appId: "YOUR_APP_ID",
+    // measurementId: "任意"
+  };
